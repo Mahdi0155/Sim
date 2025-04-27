3ac5005c5bbebdfa8867dc3c5e6914d90209e812
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-28 02:08:48 +0330

    Create check_sub.py

---
+++ utils/check_sub.py
@@ -0,0 +1,11 @@
+# utils/check_sub.py
+
+from aiogram import Bot
+from aiogram.exceptions import TelegramBadRequest
+
+async def is_subscribed(bot: Bot, user_id: int, channel_id: int) -> bool:
+    try:
+        member = await bot.get_chat_member(chat_id=channel_id, user_id=user_id)
+        return member.status in ["member", "administrator", "creator"]
+    except TelegramBadRequest:
+        return False
