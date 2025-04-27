c0a9f56857e8dd48bda15fceeb65f43e72f311b8
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-28 02:03:40 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,7 @@
 # config.py
 
 BOT_TOKEN = "7086274656:AAEpviQqS2hKPeyvsHt4rQSXIgrehlW2Ums"
-CHANNEL_ID = -1002207109791
 OWNER_ID = 6387942633
+CHANNEL_ID = -1002207109791
+
+WEBHOOK_URL = "https://your-render-app-name.onrender.com/webhook"  # اینجا آدرس وبهوک رندر رو بعداً جایگزین کن
