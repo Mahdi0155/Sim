484be6e1c1825efcb5acd2af093c468ac6edc14b
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-28 10:10:38 +0330

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,7 +1,7 @@
 from aiogram import Router
 from aiogram.types import Message
 from aiogram.filters import CommandStart
-from keyboards.subscribe import subscribe_keyboard  # <<<<<< اصلاح شد
+from keyboards.subscribe import subscribe_keyboard
 from utils.check_subscription import check_user_subscription
 from config import OWNER_ID
 
@@ -11,7 +11,8 @@ router = Router()
 async def start_command(message: Message):
     if message.from_user.id == OWNER_ID:
         return  # مدیر نیازی به این هندلر نداره
-    is_subscribed = await check_user_subscription(message.from_user.id)
+    
+    is_subscribed = await check_user_subscription(message.bot, message.from_user.id)
     if is_subscribed:
         await message.answer("خوش آمدید.")
     else:
