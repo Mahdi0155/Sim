2767d06a4420b063997f49075ef9fd9ea83cf4ac
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-28 11:13:37 +0330

    Update callbacks.py

--- handlers/callbacks.py
+++ handlers/callbacks.py
@@ -2,19 +2,21 @@
 
 from aiogram import Router, F
 from aiogram.types import CallbackQuery
-from config import CHANNEL_ID
-from utils.check_sub import is_subscribed
+from config import CHANNEL_ID, CHANNEL_LINK
+from utils.check_subscription import check_user_subscription
+from keyboards.subscribe import subscribe_keyboard
 
 router = Router()
 
-@router.callback_query(F.data == "check_sub")
+@router.callback_query(F.data == "check_subscribe")
 async def check_subscription(callback: CallbackQuery):
-    if not await is_subscribed(callback.bot, callback.from_user.id, CHANNEL_ID):
-        join_button = [{"text": "عضویت در کانال", "url": f"https://t.me/c/{str(CHANNEL_ID)[4:]}"},
-                       {"text": "بررسی عضویت", "callback_data": "check_sub"}]
+    is_subscribed = await check_user_subscription(callback.bot, callback.from_user.id)
+    if is_subscribed:
         await callback.message.edit_text(
-            "شما هنوز عضو کانال نیستید، لطفا ابتدا عضو شوید.",
-            reply_markup={"inline_keyboard": [[join_button[0]], [join_button[1]]]}
+            "✅ عضویت شما تأیید شد!\nحالا میتوانید از ربات استفاده کنید."
         )
     else:
-        await callback.message.edit_text("عضویت شما تأیید شد! حالا میتوانید فایل را دریافت کنید.")
+        await callback.message.edit_text(
+            "❗ شما هنوز عضو کانال نیستید.\nلطفا ابتدا عضو شوید.",
+            reply_markup=subscribe_keyboard()
+        )
