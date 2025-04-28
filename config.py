fe10f62873f6bbb9df4cc913a5f294608e79906e
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-28 08:41:26 +0330

    Update config.py

--- config.py
+++ config.py
@@ -4,4 +4,4 @@ BOT_TOKEN = "7086274656:AAEpviQqS2hKPeyvsHt4rQSXIgrehlW2Ums"
 OWNER_ID = 6387942633
 CHANNEL_ID = -1002207109791
 
-WEBHOOK_URL = "https://your-render-app-name.onrender.com/webhook"  # اینجا آدرس وبهوک رندر رو بعداً جایگزین کن
+WEBHOOK_URL = "https://sim-n0vy.onrender.com"  # اینجا آدرس وبهوک رندر رو بعداً جایگزین کن
