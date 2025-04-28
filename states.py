934ce6dd07807af76234e3356f2aadf1ac2b3a09
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-28 18:54:57 +0330

    Delete states.py

--- states.py
+++
@@ -1,7 +0,0 @@
-from aiogram.fsm.state import StatesGroup, State
-
-class UploadStates(StatesGroup):
-    waiting_for_caption = State()
-
-class BroadcastStates(StatesGroup):
-    waiting_for_message = State()
