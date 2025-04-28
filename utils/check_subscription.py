9ae0c57d348bece44eeb43778f4d4f62388c70a6
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-28 10:09:31 +0330

    Update check_subscription.py

--- utils/check_subscription.py
+++ utils/check_subscription.py
@@ -1,11 +1,7 @@
-# utils/check_subscription.py
-
 from aiogram import Bot
 from config import CHANNEL_ID
 
-async def check_user_subscription(user_id: int) -> bool:
-    from loader import bot
-
+async def check_user_subscription(bot: Bot, user_id: int) -> bool:
     try:
         member = await bot.get_chat_member(chat_id=CHANNEL_ID, user_id=user_id)
         return member.status in ("member", "administrator", "creator")
