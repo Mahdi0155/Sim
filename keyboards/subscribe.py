1dc90c5000c72b362545625488c53a993a26c204
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-28 11:00:10 +0330

    Update subscribe.py

--- keyboards/subscribe.py
+++ keyboards/subscribe.py
@@ -6,6 +6,6 @@ from config import CHANNEL_LINK
 def subscribe_keyboard():
     keyboard = InlineKeyboardMarkup(inline_keyboard=[
         [InlineKeyboardButton(text="عضویت در کانال", url=CHANNEL_LINK)],
-        [InlineKeyboardButton(text="بررسی عضویت", callback_data="check_subscribe")]
+        [InlineKeyboardButton(text="بررسی عضویت", callback_data="check_sub")]
     ])
     return keyboard
