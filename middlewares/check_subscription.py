daf0cfcc04f9ea877895b905dff9222511016cb4
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-28 09:26:37 +0330

    Update check_subscription.py

--- middlewares/check_subscription.py
+++ middlewares/check_subscription.py
@@ -1,17 +1,18 @@
-# middlewares/check_subscription.py
-
 from aiogram import BaseMiddleware
-from aiogram.types import Message
+from aiogram.types import Update
 from config import CHANNEL_ID
 from aiogram.exceptions import TelegramBadRequest
 
 class CheckSubscriptionMiddleware(BaseMiddleware):
-    async def __call__(self, handler, event: Message, data: dict):
+    async def __call__(self, handler, event: Update, data: dict):
+        if event.message is None:  # اگر پیام نبود (مثلا CallbackQuery یا چیز دیگه بود)، بیخیال شو
+            return await handler(event, data)
+
         try:
-            member = await event.bot.get_chat_member(CHANNEL_ID, event.from_user.id)
+            member = await event.bot.get_chat_member(CHANNEL_ID, event.message.from_user.id)
             if member.status not in ["member", "administrator", "creator"]:
                 from bot.keyboards.subscribe import subscribe_keyboard
-                await event.answer(
+                await event.message.answer(
                     "برای استفاده از ربات باید عضو کانال شوید.",
                     reply_markup=subscribe_keyboard()
                 )
