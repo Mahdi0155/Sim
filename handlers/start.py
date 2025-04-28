7a86b28613c7de29d31df396d58e8bf846ce8ff3
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-28 09:35:49 +0330

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -1,33 +1,13 @@
-# handlers/start.py
-
-from aiogram import Router, F
-from aiogram.types import Message, CallbackQuery
-from aiogram.filters import CommandStart
-from keyboards.subscribe import subscribe_keyboard
-from utils.check_subscription import check_user_subscription
-from config import OWNER_ID
-
-router = Router()
-
 @router.message(CommandStart())
 async def start_command(message: Message):
-    user_id = message.from_user.id
-
-    if user_id == OWNER_ID:
-        await message.answer("سلام ادمین عزیز!\nلطفاً یک عکس یا ویدیو ارسال کنید.")
-    else:
-        is_subscribed = await check_user_subscription(user_id)
-        if is_subscribed:
-            await message.answer("خوش آمدید! لطفاً منتظر دریافت فایل باشید.")
-        else:
-            await message.answer("برای استفاده از ربات، ابتدا در کانال عضو شوید.", reply_markup=subscribe_keyboard())
-
-@router.callback_query(F.data == "check_subscribe")
-async def check_subscribe_callback(callback: CallbackQuery):
-    user_id = callback.from_user.id
-    is_subscribed = await check_user_subscription(user_id)
-
+    if message.from_user.id == OWNER_ID:
+        return  # مدیر نیازی به این هندلر نداره، ولش کنه
+    # اینجا فقط کاربرای معمولی هندل میشن
+    is_subscribed = await check_user_subscription(message.from_user.id)
     if is_subscribed:
-        await callback.message.edit_text("عضویت شما تایید شد! لطفاً دوباره استارت کنید /start")
+        await message.answer("خوش آمدید.")
     else:
-        await callback.message.edit_text("هنوز عضو نشدید، لطفاً ابتدا عضو شوید.", reply_markup=subscribe_keyboard())
+        await message.answer(
+            "برای استفاده از ربات، ابتدا در کانال عضو شوید.", 
+            reply_markup=subscribe_keyboard()
+        )
