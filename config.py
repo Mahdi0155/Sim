d1133b2e93818fb5aaab2f9eeaa777eef4db2829
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-28 08:58:38 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,8 +1,9 @@
 # config.py
 
-BOT_TOKEN = "7086274656:AAEpviQqS2hKPeyvsHt4rQSXIgrehlW2Ums"
+BOT_TOKEN = "7086274656:AAEpviQqS2hKPeyvsHt4rQSXlGrehlW2Ums"
 OWNER_ID = 6387942633
 CHANNEL_ID = -1002207109791
 CHANNEL_LINK = "https://t.me/hottof"
 
-WEBHOOK_URL = "https://sim-n0vy.onrender.com"  # اینجا آدرس وبهوک رندر رو بعداً جایگزین کن
+# آدرس دقیق وبهوک (حتماً با /webhook در آخر)
+WEBHOOK_URL = "https://sim-n0vy.onrender.com/webhook"
