2848f63d3bb7a74e48649b45ec0c4f31c361e593
Mahdi0155 <t.mehdti21@gmail.com>
2025-04-28 18:54:41 +0330

    Delete config.py

--- config.py
+++
@@ -1,5 +0,0 @@
-BOT_TOKEN = "7086274656:AAEpviQqS2hKPeyvsHt4rQSXIgrehlW2Ums"
-OWNER_ID = 6387942633
-CHANNEL_ID = -1002207109791
-CHANNEL_LINK = "https://t.me/hottof"
-WEBHOOK_URL = "https://sim-n0vy.onrender.com/webhook"
