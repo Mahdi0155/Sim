0ad3310528bf778d23859039737be5740b6f6c5b
Mahdi0155 <t.mehdti21@gmail.com>
2025-05-01 15:47:43 +0330

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
 echo "در حال اجرای ربات اصلی..."
-python bot.py &
+python uploader_bot.py &
 
 echo "در حال اجرای ربات آپلودر..."
 python bot.py &  # برای ربات آپلودر از همون نام استفاده می‌کنیم
