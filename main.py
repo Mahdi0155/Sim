9049cbd451b6a80217fdda9a6441c070a8af6cc5
Mahdi0155 <t.mehdti21@gmail.com>
2025-05-02 18:16:56 +0330

    Update main.py

--- main.py
+++ main.py
@@ -1,12 +1,10 @@
 import uvicorn
 from fastapi import FastAPI, Request
 from aiogram import Bot, types
-from aiogram.dispatcher.webhook.aiohttp_server import SimpleRequestHandler, setup_application
 from bot import dp, bot
 from config import WEBHOOK_URL
 
 app = FastAPI()
-
 
 @app.on_event("startup")
 async def on_startup():
@@ -18,13 +16,11 @@ async def on_shutdown():
     await bot.delete_webhook()
     print("Webhook deleted")
 
-
 @app.post("/webhook")
 async def handle_webhook(request: Request):
     update = types.Update.model_validate(await request.json())
-    await dp.feed_update(bot, update)
+    await dp.feed_update(bot=bot, update=update)
     return {"ok": True}
-
 
 if __name__ == "__main__":
     uvicorn.run("main:app", host="0.0.0.0", port=10000)
