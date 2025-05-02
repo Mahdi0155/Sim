42c02081428bf9693376ffa31fd47bbbebd8419f
Mahdi0155 <t.mehdti21@gmail.com>
2025-05-02 19:01:12 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,5 +1,8 @@
-BOT_TOKEN = "7413532622:AAHVKVrh6lvVKfGav2U9L0Ehw1OoyynRJxI"
+# config.py
 
-WEBHOOK_URL = "https://sim-dtlp.onrender.com/webhook"
-
-ADMINS = [7189616405, 6387942633, 5459406429]
+BOT_TOKEN = '7413532622:AAHVKVrh6lvVKfGav2U9L0Ehw1OoyynRJxI'
+WEBHOOK_URL = 'https://sim-dtlp.onrender.com/webhook'
+ADMIN_IDS = [7189616405, 6387942633, 5459406429]
+CHANNEL_TAG = '🔥@hottof | تُفِ داغ'
+PERSISTENT_DB_PATH = 'persistent/data.db'
+PING_INTERVAL = 300
