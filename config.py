e5844c4161aa4445c9b264dd9f72da8152ea5cae
Mahdi0155 <t.mehdti21@gmail.com>
2025-05-02 19:55:01 +0330

    Update config.py

--- config.py
+++ config.py
@@ -1,3 +1,4 @@
 BOT_TOKEN = "7413532622:AAHVKVrh6lvVKfGav2U9L0Ehw1OoyynRJxI"  # فقط این مقدار رو تغییر بده
 WEBHOOK_URL = "https://sim-dtlp.onrender.com"
 ADMINS = {7189616405, 6387942633, 5459406429}
+DB_FILE = "data.json"
